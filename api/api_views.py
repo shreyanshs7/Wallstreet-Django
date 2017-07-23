ae5af12997283525a5c59d6160f47697ee9e8f26
shreyanshs7 <shreyanshss7@gmail.com>
2017-07-23 17:05:26 +0530

    updated api_views.py

--- api/api_views.py
+++ api/api_views.py
@@ -3,16 +3,33 @@ from register.models import *
 from sellbuy.models import *
 from django.core import serializers
 from django.http import JsonResponse
+import json
+
 # Create your views here.
 
 def shareprice_api(request):
-	share_data = SharePrice.objects.all()
-	share_json = serializers.serialize("json",share_data)
+	share_obj = SharePrice.objects.all()
+	share_json = serializers.serialize("json",share_obj)
+	share_data = json.loads(share_json)
+	share_list = []
+	
+	for data in share_data:
+		share_list.append(data['fields'])
 
-	return JsonResponse(share_json,safe=False)
+	share_api = json.dumps(share_list)	
+
+
+	return JsonResponse(share_api,safe=False)
 
 def users_api(request):
 	users_obj = UserDetails.objects.all()
 	users_json = serializers.serialize("json",users_obj)
+	users_data = json.loads(users_json)
+	users_list = []
 	
-	return JsonResponse(users_json,safe=False)	
+	for data in users_data:
+		users_list.append(data['fields'])
+
+	users_api = json.dumps(users_list)	
+
+	return JsonResponse(users_api,safe=False)	
